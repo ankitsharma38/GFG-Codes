b24d09bc190d928714992ef16c1ef729f4d4d93b
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2025-01-10 21:46:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sorted Array Search/sorted-array-search.java
+++ Difficulty: Basic/Sorted Array Search/sorted-array-search.java
@@ -10,22 +10,18 @@ class gfg {
         int t = Integer.parseInt(read.readLine());
 
         while (t-- > 0) {
-            String s[] = read.readLine().trim().split("\\s+");
-
-            int n = Integer.parseInt(s[0]);
-            int k = Integer.parseInt(s[1]);
-
+            String inputLine[] = read.readLine().trim().split(" ");
+            int n = inputLine.length;
             int arr[] = new int[n];
 
-            String st[] = read.readLine().trim().split("\\s+");
-
             for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(st[i]);
+                arr[i] = Integer.parseInt(inputLine[i]);
             }
+            int k = Integer.parseInt(read.readLine());
 
             Solution obj = new Solution();
 
-            System.out.println(obj.searchInSorted(arr, n, k));
+            System.out.println(obj.searchInSorted(arr, k));
         }
     }
 }
@@ -33,22 +29,21 @@ class gfg {
 
 
 class Solution {
-    static int searchInSorted(int arr[], int N, int K) {
-            int left = 0;
-        int right = N - 1;
-        
+    static boolean searchInSorted(int arr[], int k) {
+        int left = 0, right = arr.length - 1;
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            
-            if (arr[mid] == K) {
-                return 1;
-            } else if (arr[mid] < K) {
-                left = mid + 1;
+
+            if (arr[mid] == k) {
+                return true;
+            } else if (arr[mid] < k) {
+                left = mid + 1; 
             } else {
-                right = mid - 1;
+                right = mid - 1; 
             }
         }
-        
-        return -1; 
+
+        return false;
     }
 }
