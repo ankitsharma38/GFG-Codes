8fb108ef572f0f29b6e63d107a2952f7fc53c616
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2025-02-13 22:32:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Peak element/peak-element.java
+++ Difficulty: Basic/Peak element/peak-element.java
@@ -56,7 +56,7 @@ class Solution {
         while(left < right){
             int mid = left + (right - left)/2;
             if(arr[mid] < arr[mid + 1]){
-                left = mid + 1;
+                left = mid+1;
             }else{
                 right = mid;
             }
