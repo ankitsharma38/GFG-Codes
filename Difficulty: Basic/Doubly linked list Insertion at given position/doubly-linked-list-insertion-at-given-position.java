f94f169817671cb44b9ab7eeaaa2fe45bf87792a
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2025-02-22 23:54:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Doubly linked list Insertion at given position/doubly-linked-list-insertion-at-given-position.java
@@ -0,0 +1,155 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data) {
+        this.data = data;
+        next = prev = null;
+    }
+}
+
+class DLinkedList {
+
+    Node newNode(Node head, int data) {
+        Node n = new Node(data);
+        if (head == null) {
+            head = n;
+            return head;
+        }
+        head.next = n;
+        n.prev = head;
+        head = n;
+        return head;
+    }
+
+    void printList(Node node) {
+        Node temp = node;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        while (temp.prev != null) {
+            temp = temp.prev;
+        }
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        DLinkedList DLL = new DLinkedList();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t > 0) {
+            Node temp;
+            Node head = null;
+            Node root = null;
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+
+            for (int i = 0; i < n; i++) {
+                int x = Integer.parseInt(str[i]);
+                head = DLL.newNode(head, x);
+                if (root == null) root = head;
+            }
+            head = root;
+
+            String str2[] = read.readLine().trim().split(" ");
+            int pos = Integer.parseInt(str2[0]);
+            int data = Integer.parseInt(str2[1]);
+
+            Solution g = new Solution();
+            head = g.addNode(head, pos, data);
+
+            DLL.printList(head);
+            while (head.next != null) {
+                temp = head;
+                head = head.next;
+            }
+            t--;
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/* Structure of Doubly Linked List
+class Node
+{
+    int data;
+    Node next;
+    Node prev;
+    Node(int data)
+    {
+        this.data = data;
+        next = prev = null;
+    }
+}*/
+
+class Solution {
+    // Function to insert a new node at given position in doubly linked list.
+    Node addNode(Node head, int p, int x) {
+        Node newNode = new Node(x);
+        if (head == null) {
+            return newNode;
+        }
+
+        Node temp = head;
+        // Traverse to the p-th node
+        for (int i = 0; i < p; i++) {
+            if (temp.next == null) break;
+            temp = temp.next;
+        }
+
+        // Insert newNode after temp
+        newNode.next = temp.next;
+        newNode.prev = temp;
+        if (temp.next != null) {
+            temp.next.prev = newNode;
+        }
+        temp.next = newNode;
+
+        return head;
+    }
+
+    // Utility function to print the doubly linked list
+    void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        // Creating the doubly linked list: 2 <-> 4 <-> 5
+        Node head = new Node(2);
+        head.next = new Node(4);
+        head.next.prev = head;
+        head.next.next = new Node(5);
+        head.next.next.prev = head.next;
+
+        System.out.println("Original List:");
+        solution.printList(head);
+
+        // Insert 6 at position 2 (0-based index)
+        head = solution.addNode(head, 2, 6);
+
+        System.out.println("Updated List:");
+        solution.printList(head);
+    }
+}
