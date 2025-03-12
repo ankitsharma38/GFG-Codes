cb827807e2c2b5995622f745b6b8d8e827a4b15e
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2025-03-12 22:18:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Binary Search/binary-search.java
+++ Difficulty: Easy/Binary Search/binary-search.java
@@ -32,25 +32,6 @@ public class GFG {
 
 
 // this code not passes after 10 case in gfg but it conrrect run on netflix
-
-// class Solution {
-//     public int binarysearch(int[] arr, int k) {
-//         int left = 0;
-//         int right = arr.length - 1;
-        
-//         while(left <= right) {
-//             int mid = left + (right - left) / 2;
-//             if(arr[mid] == k) {
-//                 return mid;
-//             } else if(arr[mid] < k) {
-//                 left = mid + 1;
-//             } else {
-//                 right = mid - 1;
-//             }
-//         }
-//         return -1;
-//     }
-// }
 class Solution {
     public int binarysearch(int[] arr, int k) {
         int left = 0;
